9ed8a57ff72ed12592e57b8f934a124719408893
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2023-02-26 20:55:52 -0800

    initial commit

---
+++ src/lib/components/SVG.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	export let scale = 1;
+	export let rotation = 1;
+	export let skewX = 0;
+	export let skewY = 0;
+	export let fgColor = 'black';
+	export let bgColor = 'white';
+
+	const points = [
+		[0, 0],
+		[1, 0],
+		[1, 1],
+		[0, 1]
+	].map((p) => p.map((xy) => `${100 * xy}%`));
+</script>
+
+<svg viewBox="0 0 1 1" xmlns="http://www.w3.org/2000/svg">
+	<defs>
+		<pattern
+			width="100%"
+			height="100%"
+			id="pattern"
+			patternUnits="userSpaceOnUse"
+			patternTransform="scale({scale}) rotate({rotation}) skewX({skewX}) skewY({skewY})"
+		>
+			<rect width="100%" height="100%" fill={bgColor} />
+			<g fill={fgColor}>
+				{#each points as [cx, cy]}
+					<circle {cx} {cy} r="25%" />
+				{/each}
+			</g>
+		</pattern>
+	</defs>
+	<rect width="100%" height="100%" fill="url(#pattern)" />
+</svg>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import SVG from '$lib/components/SVG.svelte';
+	let scale = 0.15;
+	let rotation = 60;
+	let skewX = 0;
+	let skewY = 0;
+	let bgColor = '#99c1f1';
+	let fgColor = '#f9f06b';
+</script>
+
+<main style:background-color={bgColor} class="relative">
+	<SVG {scale} {rotation} {fgColor} {bgColor} {skewX} {skewY} />
+	<details class="absolute top-4 right-4 flex w-full max-w-sm flex-col rounded-lg bg-white/95 p-2">
+		<summary class="cursor-pointer">customize pattern</summary>
+		<label>
+			<span>background color: {bgColor}</span>
+			<input bind:value={bgColor} type="color" />
+		</label>
+		<label>
+			<span>foreground color: {fgColor}</span>
+			<input bind:value={fgColor} type="color" />
+		</label>
+		<label>
+			<span>scale: {scale}</span>
+			<input bind:value={scale} min={0.1} max={1} step={0.01} type="range" />
+		</label>
+		<label>
+			<span>rotation: {rotation}</span>
+			<input bind:value={rotation} min={0} max={360} type="range" />
+		</label>
+		<label>
+			<span>skewX: {skewX}</span>
+			<input bind:value={skewX} min={0} max={90} type="range" />
+		</label>
+		<label>
+			<span>skewY: {skewY}</span>
+			<input bind:value={skewY} min={0} max={90} type="range" />
+		</label>
+	</details>
+</main>
+
+<style lang="postcss">
+	input[type='color'] {
+		@apply rounded-lg p-1;
+	}
+	label {
+		@apply flex items-center justify-between;
+	}
+</style>

c3e61e9c6fbd0a1547cec3c227a1e48c1cd80caa
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2023-02-28 22:24:29 -0800

    Revert "add copy button, will tweak later"
    This reverts commit e01b0f7ed29cbac3eece4136941c181d4141f5b6.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,18 +2,7 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {
-			keyframes: {
-				wiggle: {
-					'0%, 100%': { transform: 'rotate(-3deg)' },
-					'50%': { transform: 'rotate(3deg)' }
-				}
-			},
-			animation: {
-				wiggle: 'wiggle 1s ease-in-out infinite'
-			}
-		}
+		extend: {}
 	},
 	plugins: []
 };
-

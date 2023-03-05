5f8c3296e908b4287a55770beaeaec541ff291e5
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2023-03-05 12:21:26 -0800

    add spiral pattern and simp circle pattern

--- src/lib/components/patterns/PolkaDots.svelte
+++ src/lib/components/patterns/PolkaDots.svelte
@@ -1,12 +1,1 @@
-<script lang="ts">
-	const points = [
-		[0, 0],
-		[1, 0],
-		[1, 1],
-		[0, 1],
-	].map((p) => p.map((xy) => `${100 * xy}%`));
-</script>
-
-{#each points as [cx, cy]}
-	<circle {cx} {cy} r="25%" />
-{/each}
+<circle cx="50%" cy="50%" r="25%" />
--- src/lib/components/patterns/index.ts
+++ src/lib/components/patterns/index.ts
@@ -1,9 +1,11 @@
 import NinjaStar from './NinjaStar.svelte';
 import PolkaDots from './PolkaDots.svelte';
+import Spiral from './Spiral.svelte';
 import Waves from './Waves.svelte';
 
 export const patterns = [
 	{ name: 'ninja star', component: NinjaStar },
 	{ name: 'polka dots', component: PolkaDots },
+	{ name: 'spiral', component: Spiral },
 	{ name: 'waves', component: Waves },
 ];

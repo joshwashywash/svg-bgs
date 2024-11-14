1e6c053f3834223aa624064a2d0251b908fc273e
josh oertel <16182568+joshwashywash@users.noreply.github.com>
2024-11-14 13:24:30 -0800

    update to svelte 5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,10 +12,20 @@
 <svelte:head>
 	<title>{$page.data.page.title}</title>
 </svelte:head>
-<meta name="description" content={$page.data.page.description} />
+<meta
+	name="description"
+	content={$page.data.page.description}
+/>
 
 {@render children?.()}
 
-<footer class="fixed bottom-4 left-4 rounded-lg bg-white p-2">
-	<a class="underline" href="https://josho.dev">josho.dev</a>
+<footer class="fixed bottom-4 left-4 rounded-lg bg-white p-2 flex gap-2">
+	<a
+		class="underline"
+		href="https://github.com/joshwashywash/svg-patterns">readme</a
+	>
+	<a
+		class="underline"
+		href="https://josho.dev">josho.dev</a
+	>
 </footer>
